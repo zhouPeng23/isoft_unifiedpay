326a06c41d9427f4a8ceadb21d70b482699593ca
zhoupeng <1875112921@qq.com>
2019-12-08 08:49:32 +0800

    申请退款返回报文结构体

--- controllers/httpRefund.go
+++ controllers/httpRefund.go
@@ -8,6 +8,9 @@ import (
 	"unifiedpay/models"
 	"github.com/astaxie/beego"
 	"strconv"
+	"github.com/astaxie/beego/httplib"
+	"crypto/tls"
+	"encoding/xml"
 )
 
 //退货请求-控制器
@@ -63,6 +66,34 @@ func (this *MainController) WeChatRefund() (string,error) {
 	}
 
 	//发送微信申请退款请求
+	logs.Info("发送申请退款求...")
+	req := httplib.Post(beego.AppConfig.String("WeChatPay_RefundApply"))
+	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	req.SetTimeout(60*time.Second,60*time.Second)
+
+	//组织xml报文
+	reqXml := RefundRequestXml{}
+	reqXml.Appid = beego.AppConfig.String("WeChatPay_Appid")
+	reqXml.Mch_id = beego.AppConfig.String("WeChatPay_MerchantNo")
+	reqXml.Nonce_str = "2ddd1a30ac87aa2db72f57a2375d8fec"
+	reqXml.Out_refund_no = order.OrderId
+	reqXml.Refund_fee = strconv.Itoa(int(order.TransAmount))
+	reqXml.Refund_fee_type = order.TransCurrCode
+	reqXml.Out_trade_no = order.OrgOrderId
+	reqXml.Total_fee = strconv.Itoa(int(orgOrder.TransAmount))
+	reqXml.Refund_desc = order.RefundReason
+	reqXml.Notify_url = beego.AppConfig.String("WeChatPay_RefNotifyUrl")
+	reqXml.Sign = "3CB01533B8C1EF103065174F50BCA002"
+
+	//设置xml报文体
+	reqXmlStr, e := xml.Marshal(reqXml)
+	logs.Info("设置xml报文体:%v",string(reqXmlStr))
+	req.XMLBody(string(reqXmlStr))
+
+	//获取返回消息、转为结构体
+	logs.Info("接收返回报文...")
+	resXmlStr, e := req.String()
+	logs.Info(fmt.Sprintf("收到报文:%v",resXmlStr))
 
 
 	return applyResult,nil
--- controllers/refundXml.go
+++ controllers/refundXml.go
@@ -5,23 +5,43 @@ import "encoding/xml"
 //申请退款请求报文结构体
 type RefundRequestXml struct {
 	XMLName  					xml.Name 				`xml:"xml"`
-	Appid 						string 					`xml:"appid"`					//公众账号ID
-	Mch_id 						string 					`xml:"mch_id"`					//商户号
-	Nonce_str					string					`xml:"nonce_str"`				//随机字符串
-	Out_refund_no				string					`xml:"out_refund_no"`			//退款订单
-	Refund_fee					string					`xml:"refund_fee"`			//本次退款金额
-	Refund_fee_type				string					`xml:"refund_fee_type"`		//退款货币种类
-	Out_trade_no				string					`xml:"out_trade_no"`			//商户订单号(原交易订单)
-	Total_fee					string					`xml:"total_fee"`				//原交易订单金额
-	Refund_desc					string					`xml:"refund_desc"`			//退款原因描述
-	Notify_url					string					`xml:"notify_url"`			//退款结果通知url
-	Sign						string					`xml:"sign"`					//签名
+	Appid 						string 					`xml:"appid"`						//公众账号ID
+	Mch_id 						string 					`xml:"mch_id"`						//商户号
+	Nonce_str					string					`xml:"nonce_str"`					//随机字符串
+	Out_refund_no				string					`xml:"out_refund_no"`				//退款订单
+	Refund_fee					string					`xml:"refund_fee"`				//本次退款金额
+	Refund_fee_type				string					`xml:"refund_fee_type"`			//退款货币种类
+	Out_trade_no				string					`xml:"out_trade_no"`				//商户订单号(原交易订单)
+	Total_fee					string					`xml:"total_fee"`					//原交易订单金额
+	Refund_desc					string					`xml:"refund_desc"`				//退款原因描述
+	Notify_url					string					`xml:"notify_url"`				//退款结果通知url
+	Sign						string					`xml:"sign"`						//签名
 }
 
 //申请退款返回报文结构体
 type RefundResponseXml struct {
 	XMLName  					xml.Name 				`xml:"xml"`
-
+	Return_code  				string 					`xml:"return_code"`				//返回状态码
+	Return_msg  				string 					`xml:"return_msg"`				//返回信息
+	Result_code  				string 					`xml:"result_code"`				//业务结果
+	Err_code	  				string 					`xml:"err_code"`					//错误代码
+	Err_code_des	  			string 					`xml:"err_code_des"`				//错误代码描述
+	Appid			  			string 					`xml:"appid"`						//公众账号ID
+	Mch_id			  			string 					`xml:"mch_id"`						//商户号
+	Nonce_str			  		string 					`xml:"nonce_str"`					//随机字符串
+	Sign				  		string 					`xml:"sign"`						//签名
+	Transaction_id				string 					`xml:"transaction_id"`			//微信订单号
+	Out_trade_no				string 					`xml:"out_trade_no"`				//商户订单号
+	Out_refund_no				string 					`xml:"out_refund_no"`				//商户退款单号
+	Refund_id					string 					`xml:"refund_id"`					//微信退款单号
+	Refund_fee					string 					`xml:"refund_fee"`				//退款金额
+	Settlement_refund_fee		string 					`xml:"settlement_refund_fee"`	//应结退款金额
+	Total_fee					string 					`xml:"total_fee"`					//标价金额
+	Settlement_total_fee 		string 					`xml:"settlement_total_fee "`	//应结订单金额
+	Fee_type 					string 					`xml:"fee_type "`					//标价币种
+	Cash_fee 					string 					`xml:"cash_fee "`					//现金支付金额
+	Cash_fee_type 				string 					`xml:"cash_fee_type "`			//现金支付币种
+	Cash_refund_fee 			string 					`xml:"cash_refund_fee "`			//现金退款金额
 }
 
 //退款结果异步通知
